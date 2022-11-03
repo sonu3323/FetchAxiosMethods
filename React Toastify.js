5de163d58d5fe5d0f637d5b2f3b537948289cbb0
Sonu sharma <53683626+sonu3323@users.noreply.github.com>
2022-11-03 20:50:06 +0530

    Create React Toastify.js
    React Toastify Global

---
+++ React Toastify.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { toast } from "react-toastify";
+
+const Toast = (message, type) => {
+  switch (type) {
+    case "success":
+      return toast.success(
+        <div>
+          <p>{message}</p>
+        </div>
+      );
+    case "error":
+      return toast.error(
+        <div>
+          <p>{message}</p>
+        </div>
+      );
+    case "warning":
+      return toast.warning(
+        <div>
+          <p>{message}</p>
+        </div>
+      );
+    default:
+      return toast.warning(
+        <div>
+          <p>Toast not defined...</p>
+        </div>
+      );
+  }
+};
+export default Toast;
+Somewhere in the component:
+
+Toast('Action successfully executed!', 'success');
